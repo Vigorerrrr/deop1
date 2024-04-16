b47f84671ec0625423a1fdf16c4e1790b14bb548
jiakefeng <kefeng.jia@normalarmor.com>
2024-04-16 18:07:43 +0800

    0416

--- patr1/class2/class1.py
+++ patr1/class2/class1.py
@@ -49,5 +49,144 @@ intern机制
 只储存包含标准字符(数字、字母、下划线)的字符串
 包含特殊字符的字符串不会被添加到大整数池中
 
-54 
+
 """
+
+"""
+深拷贝（deep copy）和浅拷贝（shallow copy）是在 Python 中用于复制对象的两种不同方式。
+
+1. **浅拷贝**：
+
+   - 浅拷贝会创建一个新的对象，但是该对象的内容是原始对象的引用。换句话说，浅拷贝创建了一个新的对象，但是其内部的子对象（如列表、字典等）仍然是原始对象中子对象的引用。
+   - 浅拷贝可以使用 `copy()` 方法来实现，或者使用切片操作符 `[:]` 进行浅拷贝。
+
+   ```python
+   import copy
+   
+   list1 = [1, 2, [3, 4]]
+   list2 = copy.copy(list1)  # 浅拷贝
+   
+   list1[0] = 5
+   list1[2][0] = 6
+   
+   print(list1)  # 输出: [5, 2, [6, 4]]
+   print(list2)  # 输出: [1, 2, [6, 4]]
+   ```
+
+2. **深拷贝**：
+
+   - 深拷贝会递归地复制对象及其所有子对象，创建一个完全独立的新对象。即使原始对象的子对象也会被复制，而不是简单地引用。
+   - 深拷贝可以使用 `copy.deepcopy()` 方法来实现。
+
+   ```python
+   import copy
+   
+   list1 = [1, 2, [3, 4]]
+   list2 = copy.deepcopy(list1)  # 深拷贝
+   
+   list1[0] = 5
+   list1[2][0] = 6
+   
+   print(list1)  # 输出: [5, 2, [6, 4]]
+   print(list2)  # 输出: [1, 2, [3, 4]]
+   ```
+
+总的来说，浅拷贝只复制对象本身，而深拷贝会复制对象及其所有子对象。在处理嵌套结构（如列表中包含列表）时，通常需要使用深拷贝以确保各个对象的独立性。
+
+
+
+
+
+
+
+
+
+
+
+
+
+拷贝应用
+深浅拷贝在实际编程中有许多应用场景，下面列举一些常见的应用：
+
+1. **列表或字典的复制**：
+
+   当需要对列表或字典进行复制，并且不想改变原始对象时，可以使用深浅拷贝。
+
+   ```python
+   import copy
+
+   original_list = [1, 2, [3, 4]]
+   
+   # 浅拷贝
+   shallow_copy_list = original_list.copy()
+   
+   # 深拷贝
+   deep_copy_list = copy.deepcopy(original_list)
+   ```
+
+2. **避免共享可变对象**：
+
+   在多个对象中共享可变对象时，可以使用深拷贝来避免共享带来的意外修改。
+
+   ```python
+   import copy
+
+   original_list = [1, 2, [3, 4]]
+   
+   # 共享原始列表的引用
+   shared_list = [original_list, original_list]
+   
+   # 对共享列表中的元素进行深拷贝
+   deep_copied_shared_list = copy.deepcopy(shared_list)
+   ```
+
+3. **处理递归数据结构**：
+
+   当处理包含自引用的数据结构时，如树或图，可以使用深拷贝来处理。
+
+   ```python
+   import copy
+
+   class Node:
+       def __init__(self, value):
+           self.value = value
+           self.children = []
+   
+   # 创建一个包含自引用的树结构
+   root = Node(1)
+   child1 = Node(2)
+   child2 = Node(3)
+   root.children = [child1, child2]
+   child1.children.append(child2)
+   
+   # 深拷贝树结构
+   copied_root = copy.deepcopy(root)
+   ```
+
+4. **备份对象状态**：
+
+   在某些情况下，需要对对象进行状态备份，以便在需要时能够回滚到先前的状态。
+
+   ```python
+   import copy
+
+   class MyObject:
+       def __init__(self, value):
+           self.value = value
+   
+   obj = MyObject(1)
+   obj_backup = copy.deepcopy(obj)  # 备份对象状态
+   
+   # 对对象进行修改
+   obj.value = 2
+   
+   # 恢复对象到先前的状态
+   obj = obj_backup
+   ```
+
+这些只是深浅拷贝在实际应用中的一些示例，它们在处理对象复制和管理数据结构时非常有用。
+"""
+
+
+
+
---
+++ patr1/class2/class2.py
@@ -0,0 +1,503 @@
+# -*- coding: utf-8 -*-
+# @file: class2.py
+# @time: 2024 
+# @user: Nemo
+"""
+垃圾回收是一种自动管理计算机程序中内存的机制，目的是识别并释放不再被程序使用的内存，以便程序能够继续有效地使用内存。在Python中，垃圾回收由解释器自动处理，不需要程序员手动管理。
+
+Python中的垃圾回收主要依靠引用计数和循环引用检测来实现。
+
+1. **引用计数**：Python中的每个对象都会维护一个引用计数，表示指向该对象的引用数量。当引用计数为0时，说明没有任何引用指向该对象，该对象就可以被销毁。Python通过增加和减少引用计数来管理内存。
+
+2. **循环引用检测**：有时候两个或多个对象之间存在循环引用，即彼此引用对方，导致它们的引用计数永远不会变为0。为了解决这个问题，Python引入了循环引用检测机制。通过检测和处理循环引用，Python可以正确地释放循环引用对象所占用的内存。
+
+除了引用计数和循环引用检测外，Python还使用了一种称为"分代回收"的策略来提高垃圾回收的效率。分代回收将对象分为不同的代，每个代对应不同的对象存活时间。通常情况下，新创建的对象会被放入年轻代，经过几次垃圾回收后仍然存活的对象会被移到老年代。由于大部分对象的生命周期很短，因此年轻代的垃圾回收频率比老年代高，这样可以提高垃圾回收的效率。
+
+总的来说，Python的垃圾回收机制通过引用计数、循环引用检测和分代回收等方式来管理内存，使得程序员可以更加方便地进行内存管理，避免内存泄漏和其他与内存相关的问题。
+
+"""
+import queue
+import random
+import threading
+import time
+import requests
+
+"""
+Python的垃圾回收机制是一种自动管理内存的机制，旨在识别和清理不再被程序使用的内存，从而释放资源并减少内存泄漏的可能性。Python的垃圾回收主要依赖于两种策略：
+
+1. **引用计数**：Python中的每个对象都有一个引用计数，用于跟踪有多少引用指向该对象。当引用计数变为零时，意味着没有任何引用指向该对象，Python会将其标记为可回收，并在适当的时候将其释放。引用计数的优点是简单高效，但它无法处理循环引用的情况。
+
+2. **循环垃圾收集**：Python的垃圾回收器也包含一个循环垃圾收集器，用于处理循环引用的情况。循环引用是指两个或多个对象之间形成的环状引用关系，导致它们的引用计数永远不会降为零。Python的垃圾回收器通过检测和清理这种循环引用，来释放这些对象占用的内存。
+
+Python的垃圾回收器使用了分代收集的策略，将对象分为不同的代，每个代拥有不同的存活周期。新创建的对象被放入年轻代，而经过多次垃圾回收仍然存活的对象则会被提升到下一个代。这种分代收集策略可以提高垃圾回收的效率，因为大部分对象的生命周期较短，而只有少数对象会存活较长时间。
+
+总体来说，Python的垃圾回收机制通过引用计数、循环垃圾收集和分代收集等策略来管理内存，从而确保程序能够高效地使用内存资源，减少内存泄漏的可能性。
+
+"""
+
+"""
+python进阶(五):并发和性能
+一、并发和并行
+1、多任务
+cup与多任务的天系:
+单核CPU可不可以执行多任务?
+也可以执行多任务,由于CPU执行代码都是顺序执行的,那么,单核CPU是怎么执行多任务的呢?答案就
+是操作系统轮流让各个任务交替执行,任务1执行0.01秒,切换到任务2,任务2执行0.01秒,再切换到任
+务3,执行0.01秒......这样反复执行下去,表面上看,每个任务都是交替执行的,但是,由于CPU的执行
+速度实在是太快了,我们感觉就像所有任务都在同时执行一样。
+真正的并行执行多任务只能在多核CPU上实现,但是,由于任务数量远远多于CPU的核心数量,所以,操作系
+统也会自动把很多任务轮流调度到每个核心上执行。
+
+
+2、并发和并行:
+并发:指的是任务数多余cpu核数,通过操作系统的各种任务调度算法,实现用多个任务"一起"执行(实际上
+总有一些任务不在执行,因为切换任务的速度相当快,看上去一起执行而已
+并行:指的是任务数小于等于cpu核数,即任务真的是一起执行的
+
+
+总结
+真正的并行执行多任务只能在多核CPU上实现,但是,由于任务数量远远多于CPU的核心数量,所以,操
+作系统也会自动把很多任务轮流调度到每个核心上执行。
+
+并发:指的是任务数多余cpu核数,通过操作系统的各种任务调度算法,实现用多个任务"一起"执行(实
+际上总有一些任务不在执行,因为切换任务的速度相当快,看上去一起执行而已
+并行:指的是任务数小于等于cpu核数,即任务真的是一起执行的
+
+
+"""
+
+"""
+进程 线程 协程
+
+
+threading 模块
+创建线程:threading.Thread(target=func)
+
+Thread类提供了以下方法:
+run()       用以表示线程活动的方法
+start()     启动线程活动
+join([time])        设置主线程会等待time秒后再往下执行,time默认为子线程结束,多个子线程之间设置的值会叠加
+isAlive()       返回线程是否活动的
+getName()       返回线程名
+setName()       设置线程名。
+
+
+
+"""
+# import threading
+# import time
+#
+#
+# # 定义一个函数，作为线程要执行的任务
+# def task(name, delay):
+#     print(f"Thread {name} is starting...")
+#     time.sleep(delay)  # 模拟任务执行时间
+#     print(f"Thread {name} is done.")
+#
+#
+# # 创建线程对象，并传入要执行的任务
+# thread1 = threading.Thread(target=task, args=("Thread 1", 2))  # 任务名为Thread 1，延迟2秒执行
+# thread2 = threading.Thread(target=task, args=("Thread 2", 3))  # 任务名为Thread 2，延迟3秒执行
+#
+# # 启动线程
+# thread1.start()
+# print('\n',thread1.name)
+# thread2.start()
+#
+# # 主线程等待所有子线程执行完成
+# thread1.join()
+# thread2.join()
+#
+# print("All threads are done.")
+#
+#
+#
+"""
+class RequestThread(threading.Thread):
+    # 发送requests请求
+
+    def __init__(self, url):
+        self.url = url
+        super().__init__()
+
+    def run(self):
+        for i in range(10):
+            res = requests.get(self.url)
+            print('线程:{}--返回的状态码-{}--'.format(threading.current_thread(), res.status_code))
+
+
+# 创建五个线程,发起请求
+s_time = time.time()
+for i in range(5):
+    t = RequestThread(url='www.dd.com')
+    t.start()
+e_time=time.time()
+print('耗时:',e_time-s_time)
+
+传参数则需要重写_init_方法(注意重写之后要调用父类)的init方法)
+
+
+"""
+
+
+"""
+3、多线程-共享全局变量
+多线程之间修改全局变量
+线程之间是共用同一块内存的,那么线程可以共享全局变量
+案例:当前有一个全局变量a=100,再线程a中修改,线程b中是否会生效,
+列表当做实参传递到线程中,线程中修改会有说明影响
+
+总结
+在一个进程内的所有线程共享全局变量,很方便在多个线程间共享数据
+缺点就是,线程是对全局变量随意遂改可能造成多线程之间对全局变量的混乱(即线程非安全)
+
+
+#多线程全局变量的问题
+#全局变量
+a = 100
+
+
+def func1():
+    global a
+    for i in range(200000000):
+        meta.acquire()
+        a += 1
+        meta.release()
+    print('线程1修改完',a)
+
+
+def func2():
+    global a
+    for i in range(200000000):
+        meta.acquire()
+        a += 1
+        meta.release()
+    print('线程2修改完',a)
+
+
+t1 = threading.Thread(target=func1)
+
+t2 = threading.Thread(target=func2)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+print(a)
+
+"""
+
+
+
+"""
+同步
+同步就是协同步调,按预定的先后次序进行运行。"同"字从字面上容易理解为一起动作,其实不
+是,"同"字应是指协同、协助、互相配合,如进程、线程同步,可理解为进程或线程A和B一块配合,A执
+行到一定程度时要依靠B的某个结果,于是停下来,示意B运行;B执行再将结果给A:A再继续操作。
+
+
+·互斥锁
+线程同步能够保证多个线程安全访问竞争资源,最简单的同步机制是引入互斥锁。
+。互斥锁为资源引入一个状态:锁定/非锁定。
+。某个线程要更改共享数据时,先将其锁定,此时资源的状态为"锁定",其他线程不能更改直到该线程释放
+资源,将资源的状态变成"非锁定",其他的线程才能再次锁定该资源
+。互斥锁保证了每次只有一个线程进行写入操作,从而保证了多线程情况下数据的正确性
+threading模块中定义了Lock类,可以方便的处理锁定:
+
+
+meta = threading.Lock()
+
+
+
+
+
+a = 100
+
+
+def func1():
+    global a
+    for i in range(20000000):
+        meta.acquire()
+        a += 1
+        meta.release()
+    print('线程1修改完',a)
+
+
+def func2():
+    global a
+    for i in range(20000000):
+        meta.acquire()
+        a += 1
+        meta.release()
+    print('线程2修改完',a)
+
+
+meta = threading.Lock()
+t1 = threading.Thread(target=func1)
+
+t2 = threading.Thread(target=func2)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+print(a)
+
+
+"""
+
+"""
+GIL讨论题如下
+描述PythonGIL的概念,以及它对python多线程的影响?
+Guido的声明:http://www.artima.com/forums/flat.jsp?foruim=106&thread=214235
+参考答案:
+I
+1.Python语言和GIL没有半毛钱关系。仅仅是由于历史原因在Cpython虚拟机(解释器),难以移除GIL。
+
+2.GIL:全局解释器锁,每个线程在执行的过程都需要先获取GIL,保证同一时刻只有一个线程可以执行代码.
+
+3.线程释放GIL锁的情况:在IO操作等可能会引起阻塞的systemcall之前,可以暂时释放GIL,但在执行完毕
+后,必须重新获取GILPython3.x使用计时器(执行时间达到间值后后,当前线程释放GIL)或Python2.x,tickets计数达到100
+
+4.Python使用多进程是可以利用多核的CPU资源的。
+
+
+
+
+7、GIL全局解释器锁(扩展)
+·问题一:python单线程和多线程分别来完成工作,到底哪个快??
+
+io密集型:涉及到网络、磁盘10的任务都是IO密集型任务,这类任务的特点是CPU消耗很少,任务的大部
+分时间都在等待IO操作完成(因为IO的速度远低于CPU和内存的速度)
+
+cup密集型:cup密集型也称为计算密集型,任务的特点是要进行2大量的计算,消耗CPU资源,比如计算
+圆周率、对视频进行高清解码等等,全靠CPU的运算能力
+
+
+
+
+"""
+"""
+三、队列
+Python的Queue模块中提供了同步的、线程安全的队列类,包括:FIRFO(先入先出)队列 Queue LIFO(后
+入先出)队列LifoQueue,优先级队列PriorityQueue,这些队列都实现了锁原语,能够在多线程中直接使
+用,可以使用队列来实现线程间的同步。
+初始化Queue()对象时(例如:q=Queue()),若括号中没有指定最大可接收的消息数量,或数量为负值,
+那么就代表可接受的消息数量没有上限
+队列的方法:
+def task_done(self):...
+def join(self):...
+def qsize(self):...
+def empty(self):...
+def full(self):..
+def put(self, item,block=True,timeout=None):...
+def get(self,block=True,timeout=None):...
+def put_nowait(self,item):...
+def get_nowait(self):..
+
+
+Queue.qsize():返回当前队列包含的消息数量;
+Queue.empty()如果队列为空,返回True,反之False
+Queue.full()如果队列里队列满了,返回True反之False
+Queue.full()获取队列,timeout等待时间
+    如果block表示是否等待,如果timeout表示等待时间,
+    get(self,block=True,timeout=None)
+Queue.put(item)写入队列
+    #如果block表示是否等待,如果timeout表示等待时间,
+    put(self,item,block=True,timeout=None)
+Queueue.get_nowait()相当Queue.get(False)
+Queue.put_nowait(item) 相当Queue.put(item, False)
+Queue.task_done()在完成一项工作之后,使用Queue.task_done()方法可以向队列发送一个信号,表
+示该任务执行完毕
+
+Queue.join()实际上意味着等到队列中所有的任务(数据)执行完毕之后,再往下,否则一直等待
+    注意点:join()是判断的依据,不单指的是队列中没有数据,数据get出去之后,要使用
+    task_done()向队列发送一个信号,表示该任务执行(数据使用)完毕,
+
+
+
+
+
+"""
+
+
+"""
+先入先出
+q = queue.Queue(3)
+q.put(1)
+q.put(11)
+q.put(111)
+
+print(q.get())
+print(q.get())
+
+
+
+import threading
+import time
+import random
+
+
+# 生产者类
+class Producer(threading.Thread):
+    def __init__(self, q):
+
+        self.queue = q
+        super().__init__()
+
+    def run(self):
+        while True:
+            
+            item = random.randint(0, 100)  # 生产一个随机数
+            self.queue.put(item)  # 将产品放入队列
+
+            print(f'生产者生产了 {item}')
+            time.sleep(random.random())  # 模拟生产耗时
+
+
+# 消费者类
+class Consumer(threading.Thread):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+
+    def run(self):
+        while True:
+            item = self.queue.get()  # 从队列中取出产品
+            if item is None:
+                break  # 如果取出的是None，表示队列已空，线程结束
+            print(f'消费者消费了 {item}')
+            time.sleep(random.random())  # 模拟消费耗时
+            self.queue.task_done()  # 通知队列任务完成
+
+
+# 主函数
+def main():
+    q = queue.Queue()  # 创建一个队列
+
+    # 创建生产者线程和消费者线程
+    producer = Producer(q)
+    consumer = Consumer(q)
+
+    # 启动线程
+    producer.start()
+    consumer.start()
+
+    # 主线程等待消费者线程结束
+    consumer.join()
+
+    # 当消费者线程结束时，发送None信号给生产者线程，使其也结束
+    q.put(None)
+    producer.join()
+
+
+if __name__ == '__main__':
+    main()
+
+"""
+
+"""
+
+q = queue.Queue()
+
+class Producer(threading.Thread):
+    def run(self):
+    # 判断队列中的商品数是否少于50,少于50了之后就生产200个
+        count=0
+        while True:
+            if q.qsize()<50:
+                for i in range(200):
+                    count +=1
+                    goods='--第{}个商品---'.format(count)
+                    q.put(goods)
+                    print("生产:",goods)
+            time.sleep(1)
+
+
+
+class Consumer(threading.Thread):
+
+    def run(self):
+        while True:
+            if q.qsize() > 10:
+                for i in range(3):
+                    print('消费:{}'.format(q.get()))
+            time.sleep(0.1)
+
+
+
+p = Producer()
+p.start()
+for i in range(5):
+    c = Consumer()
+    c.start()
+
+"""
+
+
+import threading
+import queue
+import time
+import random
+
+# 共享队列
+shared_queue = queue.Queue()
+# 控制生产者和消费者的运行状态
+running = True
+# 互斥锁，用于保护共享资源
+lock = threading.Lock()
+
+# 生产者线程函数
+def producer():
+    global running
+    while running:
+        # 随机生成数据
+        data = random.randint(1, 100)
+        with lock:
+            # 将数据放入队列
+            shared_queue.put(data)
+            print("Produced:", data)
+        time.sleep(random.random())  # 模拟生产过程中的耗时操作
+
+# 消费者线程函数
+def consumer():
+    global running
+    while running:
+        with lock:
+            if not shared_queue.empty():
+                # 从队列中获取数据
+                data = shared_queue.get()
+                print("Consumed:", data)
+        time.sleep(random.random())  # 模拟消费过程中的耗时操作
+
+# 创建生产者线程和消费者线程
+producer_thread = threading.Thread(target=producer)
+consumer_thread = threading.Thread(target=consumer)
+
+# 启动线程
+producer_thread.start()
+consumer_thread.start()
+
+# 等待一段时间后停止生产者和消费者
+time.sleep(10)
+running = False
+
+# 等待线程结束
+producer_thread.join()
+consumer_thread.join()
+
+print("Producer and Consumer stopped.")
+"""
+14:50
+"""
+
+
+
+
+
+
+
